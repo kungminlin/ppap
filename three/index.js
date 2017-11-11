a9fe55284dd58fb34a4fc713a35d1055d559e17c
kungminlin <mml0816@gmail.com>
2017-11-10 16:11:22 -0800

    First commit

---
+++ three/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+  app.use('/public' ,express.static(__dirname + '/public'));
+});
+
+var cubes = [];
+var users = [];
+
+io.on('connection', function(socket) {
+  console.log(socket.id + ' has connected');
+  socket.emit('client connect');
+  socket.emit('objects', cubes);
+  socket.on('add object', function(obj){
+    console.log("creating object at " + obj.x + ", " + obj.y + ", " + obj.z + ", with color " + obj.color);
+    io.sockets.emit('add object', {x:obj.x, y:obj.y, z:obj.z, color:obj.color});
+  });
+  socket.on('objects', function(obj){
+    cubes.push(obj);
+    io.sockets.emit('objects', cubes);
+  });
+  socket.on('add user', function(coords){
+    console.log("user located at " + coords.x + ", " + coords.y + ", " + coords.z + ", with color " + coords.color)
+    users.push(coords);
+    io.sockets.emit('users', coords);
+  });
+  if (typeof users !== 'undefined' && users.length > 0) {
+    var n = Math.floor(Math.random()*users.length);
+    console.log(n);
+    socket.emit('camera target', users[n]);
+  }
+  socket.on('disconnect', function(){
+    console.log(socket.id + ' has disconnected');
+  });
+});
+
+// var userNum = 0;
+
+// io.on('connection', function(socket){
+//   var addedUser = false;
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+//   socket.on('get user', function(username){
+//     if (addedUser) {
+//       io.emit('added user', username);
+//       return;
+//     }
+//     if (username.trim() === "") username="Anonymous";
+//     socket.username = username;
+//     ++userNum;
+//     addedUser = true;
+//     io.emit('get user', username);
+//     io.emit('added user', username);
+//   });
+//   socket.on('chat message', function(msg){
+//     console.log(socket.username + ': ' + msg);
+//     io.emit('chat message', msg);
+//   });
+// });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
